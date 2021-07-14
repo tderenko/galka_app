9e31e9b0e206f41e37a5aca8cc961d4d279deaf1
tderenko <tarasid23@gmail.com>
2021-07-14 18:45:51 +0300

    added scroll view

--- resource/views/Views.py
+++ resource/views/Views.py
@@ -9,7 +9,7 @@ from resource.services.Pagination import Pagination
 import html
 import re
 
-#Window.size = (480, 853)
+Window.size = (480, 853)
 
 
 class View(GridLayout):
@@ -18,6 +18,7 @@ class View(GridLayout):
         self.content = BoxLayout()
         self.content.orientation = 'vertical'
         self.current_cat = 33
+        self.per_page = 20
         self.categories = Galka().get_categories()
         self.posts = list()
         self.build_posts(cat_id=self.current_cat)
@@ -29,11 +30,13 @@ class View(GridLayout):
 
         if load:
             cat_id = cat_id if cat_id else self.current_cat
-            self.posts = Galka().get_posts(cat_id=cat_id, page=current_page)
+            self.posts = Galka().get_posts(cat_id=cat_id, page=current_page, per_page=self.per_page)
 
         for post in self.posts['posts']:
             article = Item(id=post['id'], text=post['title']['rendered'])
             article.bind(on_release=self.build_post)
+            article.size_hint_y = None
+            article.size = (200, 70)
             self.content.add_widget(article)
 
         pagination = Pagination(data={
@@ -41,8 +44,7 @@ class View(GridLayout):
             'limit': int(self.posts['per_page']),
             'currentPage': int(self.posts['page'])
         }).run()
-
-        pagination_layout = BoxLayout()
+        self.pagi_section.clear_widgets()
         for page in pagination['pages']:
             but = Item(id=page['page'], text=str(page['label']))
             if page['class'] == 'current':
@@ -50,10 +52,11 @@ class View(GridLayout):
             else:
                 but.background_color = (0.4, 0.7, 0.2, 1)
             but.bind(on_release=self.select_pagination)
-            pagination_layout.add_widget(but)
-        self.content.add_widget(pagination_layout)
+            self.pagi_section.add_widget(but)
+
 
     def build_category(self):
+        self.pagi_section.clear_widgets()
         self.content.clear_widgets()
         for category in self.categories:
             cat = Item(id=category['id'], text=category['name'])
@@ -62,12 +65,14 @@ class View(GridLayout):
             self.content.add_widget(cat)
 
     def build_post(self, post):
+        self.pagi_section.clear_widgets()
         self.content.clear_widgets()
         post_data = Galka().get_post(id=post.id)
-        self.content.add_widget(Button(text="Return Back!", on_release=self.return_back, background_color=(1, 1, 0.5, 1), size_hint=(0.5, 0.1), halign='center'))
-        self.content.add_widget(Label(text=html.unescape(post_data['title']['rendered']), color=(0.5, 1, 0.5, 1), size_hint=(1, 0.2)))
-        text = re.sub(re.compile('<.*?>'), '', html.unescape(post_data['content']['rendered']))
-        self.content.add_widget(Label(text=text))
+        self.content.add_widget(Button(text="Return Back!", on_release=self.return_back, background_color=(1, 1, 0.5, 1), size_hint=(0.5, None), height=40, halign='center'))
+        self.content.add_widget(Label(text=html.unescape(post_data['title']['rendered']), color=(0.5, 1, 0.5, 1), size_hint=(1, 0.1)))
+        text = Label(text=re.sub(re.compile('<.*?>'), '', html.unescape(post_data['content']['rendered'])))
+        text.bind(size=text.setter('text_size'))
+        self.content.add_widget(text)
 
     def click_menu(self):
         self.show_menu = not self.show_menu
